c273b6743fb887b6e21f18fe399ac66c2d8a7604
atpn67 <andrea.topan@outlook.it>
2026-05-30 17:02:08 +0200

    working step 1

--- controllo_PID.ino
+++ controllo_PID.ino
@@ -5,7 +5,8 @@
 
 // pin list
 #define PIN_ENCODER   2
-#define PIN_PWM       9
+#define PIN_PWM_OUT   9
+#define PIN_TRIMMER   0
 
 // timeout and cycle count
 #define CYCLE_TIME_MS 100
@@ -16,50 +17,39 @@
 uint32_t tNext_ms = 0;
 uint32_t tCurr_ms = 0;
 
-volatile long pulse_count = 0;
+uint32_t val_trimmer = 0;
+uint32_t pwm_setpoint = 0;
 
-
-/***
-
-
-void setup() {
-  pinMode(2, INPUT_PULLUP);  // Pin dell'encoder
-  attachInterrupt(digitalPinToInterrupt(2), contaImpulso, RISING);
-  Serial.begin(9600);
-}
-
-void loop() {
-  Serial.println(impulsi);
-  delay(100);
-}
-
-void contaImpulso() {
-  impulsi++;
-}
-****/
-
+volatile uint32_t pulse_count = 0;
+uint32_t num_pulse = 0;
+float speed_rpm = 0;
 
 void setup() 
 {
   Serial.begin(115200);
-
+  
   pinMode(LED_BUILTIN, OUTPUT);
   // Pin dell'encoder
   pinMode(PIN_ENCODER, INPUT_PULLUP);  
-  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER), isr_pulse_count, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER), isr_pulse_count, RISING);
 
   tCurr_ms = millis();
   tNext_ms = tCurr_ms + CYCLE_TIME_MS;
 
+  // read starting setpoint
+  val_trimmer = analogRead(PIN_TRIMMER);
+  pwm_setpoint = val_trimmer/4;
+  //analogWrite(PIN_PWM_OUT, pwm_setpoint);
 }
 
 void loop() 
 {
-  analogWrite(PIN_PWM, 127);
-
   static uint32_t loop_count = 0;
 
   tCurr_ms= millis();
+
+  //analogWrite(PIN_PWM_OUT, pwm_setpoint);
+  analogWrite(PIN_PWM_OUT, 200);
 
   if (tCurr_ms >= tNext_ms)
   {
@@ -83,16 +73,31 @@ void loop()
 // commento
 void task_100ms()
 {
+  // command motor speed
+  //analogWrite(PIN_PWM_OUT, pwm_setpoint);
 
+  // get actual motor speed
+  num_pulse = pulse_count;
+  pulse_count = 0;
+  // speed_rpm = ((num_pulse * 60 * 1000)/(1000*12));
+  //speed_rpm = ((num_pulse * 60000)/12000);
+  speed_rpm = ((num_pulse * 60000)/1200);
+
+  // get new rpm setpoint value
+  val_trimmer = analogRead(PIN_TRIMMER);
+  pwm_setpoint = val_trimmer/4;
 }
 
 // commento
 void task_1000ms()
 {
-  long num_pulse = pulse_count;
-  pulse_count = 0;
-  Serial.print("conteggio: ");
-  Serial.println(num_pulse, DEC);
+  //Serial.print("conteggio: ");
+  //Serial.println(num_pulse, DEC);
+
+  Serial.print("setpoint: ");
+  Serial.println(pwm_setpoint, DEC);
+  Serial.print("velocità: ");
+  Serial.println(speed_rpm, 1);
 }
 
 void doLedlink()
